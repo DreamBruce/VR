bde02f261cd4abf276ec9e90542b07fed13f218e
DreamBruce <dreamchen0917@gmail.com>
2024-11-01 11:56:46 +0800

    3

---
+++ outletfood/data.js
@@ -0,0 +1,254 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "春水堂",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9306181674457008,
+        "pitch": -0.11448671539250022,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9091452060404883,
+          "pitch": 0.3544725900828194,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.9715159640657323,
+          "pitch": 0.3187253344738785,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.594622063117594,
+          "pitch": -0.2800095498607078,
+          "title": "春水堂",
+          "text": "主要是賣中式茶飲，也有賣主餐像是麵類或是飯"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "美食街",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.087043661766412,
+        "pitch": -0.04190387932489692,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9648267289648089,
+          "pitch": 0.029682563962932562,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5159907522375313,
+          "pitch": 0.0827438843955619,
+          "title": "美食街",
+          "text": "這裡主打的是各式各樣的飲食有拉麵、海南雞飯、火鍋、豆花"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "壽司店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.2976669344379896,
+        "pitch": 0.08304139280272871,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.761961728907224,
+          "pitch": 0.14148450256844036,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7036379265351336,
+          "pitch": 0.07462470707188018,
+          "title": "藏壽司",
+          "text": "特色是吃五盤壽司就可以玩一次遊戲，中獎後會得到扭蛋"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "風月",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.95677859380588,
+        "pitch": -0.11755583624554156,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.032546609578453456,
+          "pitch": 0.028444127232052807,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.121741254740096,
+          "pitch": 0.2665995307900779,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2832156866392772,
+          "pitch": -0.24862766951067172,
+          "title": "鶴橋風月",
+          "text": "桌子中間有一個大鐵板那裡的服務員會在鐵板上為你煎大阪燒"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "乾杯",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9671382571179086,
+        "pitch": -0.021844544334937765,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0268354322953215,
+          "pitch": 0.29386035737866223,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.3732797292689938,
+          "pitch": 0.4126451923177328,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8992391842812122,
+          "pitch": -0.28890490137434455,
+          "title": "乾杯",
+          "text": "朋友聚會可以來這裡，或是生日時來這裡慶生，那裡的服務生都很熱情"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
