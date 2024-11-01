7282775689d7fa0a71b6c92191c03292b744fd1a
DreamBruce <dreamchen0917@gmail.com>
2024-11-01 11:34:17 +0800

    e

---
+++ outletother/data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "中庭",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.879051060397252,
+        "pitch": 0.03754531057570887,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8176857974453995,
+          "pitch": -0.07327162263469056,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.3767088906977829,
+          "pitch": 0.1321956900987793,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.10691176208218778,
+          "pitch": 0.13415121631533466,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.075854150106771,
+          "pitch": 0.04094249303826558,
+          "title": "舞台",
+          "text": "有時這裡會辦小演唱會或是表演"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "衣服",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.216182120093385,
+        "pitch": 0.10876779467743702,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.36586782068203405,
+          "pitch": 0.14182698958776285,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4451309194576663,
+          "pitch": -0.26185297078475855,
+          "title": "UNIQLO",
+          "text": "一年四季所穿的衣服都有賣，夏天的短褲到冬天的大衣都有"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "誠品",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.5385120897753914,
+        "pitch": 0.051419665959816285,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2287417070982922,
+          "pitch": 0.3915419034732004,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.04669597232050293,
+          "pitch": 0.12545445662935606,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.4735850537427613,
+          "pitch": 0.11110376682802503,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.7597507517812137,
+          "pitch": 0.2560089492880806,
+          "title": "誠品書店",
+          "text": "裡面不只有賣書還有文具、書包和兒童區"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "超市",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6389580721649786,
+        "pitch": -0.02958115378692483,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0535585293066028,
+          "pitch": 0.05397064601058332,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5304392947685503,
+          "pitch": -0.27878572439424687,
+          "title": "超市",
+          "text": "裡面有賣各式各樣得食物，例如牛奶、雞肉、青菜等"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "鞋店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.9842198043342734,
+        "pitch": -0.013747554068467949,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8456323703871025,
+          "pitch": 0.3416413717707414,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.45344629838862893,
+          "pitch": -0.19948417072255253,
+          "title": "NIKE",
+          "text": "裡面有賣運動衣服、籃球鞋、跑步鞋"
+        },
+        {
+          "yaw": -2.7535356595142417,
+          "pitch": 0.037769845242211275,
+          "title": "ADIDAS",
+          "text": "有賣許多跑步鞋、籃球鞋，也有賣一些運動衣和運動用品，還有一個潮流穿搭區"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
